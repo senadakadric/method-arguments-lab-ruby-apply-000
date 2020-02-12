f9a3952cb66228004c7ec7d70f8e9f347d8b3fe6
Senada Kadric <skadric@fordham.edu>
2020-02-12 12:22:13 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,3 @@
 def introduction_with_language(name, language)
-puts "Hi, my name is #{name} and I am
-learning to program in #{language}.\n"
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
